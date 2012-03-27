8232ab1b7be159b5679f7d0c29c3574389aa20ea
Daniel Friesen <pub-github@nadir-seen-fire.com>
2012-03-27 13:13:47 -0700

    Basic skin fix up so it just barely runs in recent MediaWiki: - Move roundedsassy.php to RoundedSassy.skin.php - Add new roundedsassy.php entry file for the require_once from LocalSettings.php - Move roundedsassy/ to css/ - Add the i18n file with skin name and description fix error from removal of tooltipAndAccesskey

---
+++ RoundedSassy.i18n.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Internationalization file for skin Rounded Sassy.
+ *
+ * @file
+ * @ingroup Skins
+ */
+
+$messages = array();
+
+/** English
+ */
+$messages['en'] = array(
+	'skinname-roundedsassy' => "Rounded Sassy",
+	'roundedsassy-desc' => "Rounded Blue with a new color scheme and no background image at the top.",
+);
---
+++ RoundedSassy.skin.php
@@ -0,0 +1,410 @@
+<?php
+/**
+ * roundedsassy 
+ *
+ */
+
+if( !defined( 'MEDIAWIKI' ) )
+	die( -1 );
+
+/**
+ * Inherit main code from SkinTemplate, set the CSS and template filter.
+ * @todo document
+ * @ingroup Skins
+ */
+class SkinRoundedSassy extends SkinTemplate {
+	/** Using roundedsassy. */
+	function initPage( OutputPage $out ) {
+		parent::initPage( $out );
+		$this->skinname  = 'roundedsassy';
+		$this->stylename = 'roundedsassy';
+		$this->template  = 'RoundedSassyTemplate';
+
+	}
+
+	function setupSkinUserCss( OutputPage $out ) {
+		global $wgHandheldStyle;
+
+		parent::setupSkinUserCss( $out );
+
+		// Append to the default screen common & print styles...
+		$out->addStyle( 'roundedsassy/css/main.css', 'screen' );
+		if( $wgHandheldStyle ) {
+			// Currently in testing... try 'chick/main.css'
+			$out->addStyle( $wgHandheldStyle, 'handheld' );
+		}
+
+		$out->addStyle( 'roundedsassy/css/IE50Fixes.css', 'screen', 'lt IE 5.5000' );
+		$out->addStyle( 'roundedsassy/css/IE55Fixes.css', 'screen', 'IE 5.5000' );
+		$out->addStyle( 'roundedsassy/css/IE60Fixes.css', 'screen', 'IE 6' );
+		$out->addStyle( 'roundedsassy/css/IE70Fixes.css', 'screen', 'IE 7' );
+		$out->addStyle( 'roundedsassy/css/IE80Fixes.css', 'screen', 'IE 8' );
+
+		$out->addStyle( 'roundedsassy/css/rtl.css', 'screen', '', 'rtl' );
+	}
+}
+
+/**
+ * @todo document
+ * @ingroup Skins
+ */
+class RoundedSassyTemplate extends QuickTemplate {
+	var $skin;
+	/**
+	 * Template filter callback for roundedsassy skin.
+	 * Takes an associative array of data set from a SkinTemplate-based
+	 * class, and a wrapper for MediaWiki's localization database, and
+	 * outputs a formatted page.
+	 *
+	 * @access private
+	 */
+	function execute() {
+		global $wgRequest;
+		$this->skin = $skin = $this->data['skin'];
+		$action = $wgRequest->getText( 'action' );
+
+		// Suppress warnings to prevent notices about missing indexes in $this->data
+		wfSuppressWarnings();
+
+?><!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="<?php $this->text('xhtmldefaultnamespace') ?>" <?php
+	foreach($this->data['xhtmlnamespaces'] as $tag => $ns) {
+		?>xmlns:<?php echo "{$tag}=\"{$ns}\" ";
+	} ?>xml:lang="<?php $this->text('lang') ?>" lang="<?php $this->text('lang') ?>" dir="<?php $this->text('dir') ?>">
+	<head>
+		<meta http-equiv="Content-Type" content="<?php $this->text('mimetype') ?>; charset=<?php $this->text('charset') ?>" />
+		<?php $this->html('headlinks') ?>
+		<title><?php $this->text('pagetitle') ?></title>
+		<?php $this->html('csslinks') ?>
+
+		<!--[if lt IE 7]><script type="<?php $this->text('jsmimetype') ?>" src="<?php $this->text('stylepath') ?>/common/IEFixes.js?<?php echo $GLOBALS['wgStyleVersion'] ?>"></script>
+		<meta http-equiv="imagetoolbar" content="no" /><![endif]-->
+
+		<?php print Skin::makeGlobalVariablesScript( $this->data ); ?>
+
+		<script type="<?php $this->text('jsmimetype') ?>" src="<?php $this->text('stylepath' ) ?>/common/wikibits.js?<?php echo $GLOBALS['wgStyleVersion'] ?>"><!-- wikibits js --></script>
+		<!-- Head Scripts -->
+<?php $this->html('headscripts') ?>
+<?php	if($this->data['jsvarurl']) { ?>
+		<script type="<?php $this->text('jsmimetype') ?>" src="<?php $this->text('jsvarurl') ?>"><!-- site js --></script>
+<?php	} ?>
+<?php	if($this->data['pagecss']) { ?>
+		<style type="text/css"><?php $this->html('pagecss') ?></style>
+<?php	}
+		if($this->data['usercss']) { ?>
+		<style type="text/css"><?php $this->html('usercss') ?></style>
+<?php	}
+		if($this->data['userjs']) { ?>
+		<script type="<?php $this->text('jsmimetype') ?>" src="<?php $this->text('userjs' ) ?>"></script>
+<?php	}
+		if($this->data['userjsprev']) { ?>
+		<script type="<?php $this->text('jsmimetype') ?>"><?php $this->html('userjsprev') ?></script>
+<?php	}
+		if($this->data['trackbackhtml']) print $this->data['trackbackhtml']; ?>
+	</head>
+<body<?php if($this->data['body_ondblclick']) { ?> ondblclick="<?php $this->text('body_ondblclick') ?>"<?php } ?>
+<?php if($this->data['body_onload']) { ?> onload="<?php $this->text('body_onload') ?>"<?php } ?>
+ class="mediawiki <?php $this->text('dir') ?> <?php $this->text('pageclass') ?> <?php $this->text('skinnameclass') ?>">
+	<div id="globalWrapper">
+    <div id="masthead">
+	<h1>Rounded Sassy MediaWiki Skin</h1>
+	<h2>Your tagline here</h2>
+	</div>
+		<div id="column-content">
+	<div id="content">
+		<a name="top" id="top"></a>
+		<?php if($this->data['sitenotice']) { ?><div id="siteNotice"><?php $this->html('sitenotice') ?></div><?php } ?>
+        <div id="td-roundedbox">
+<div class="td-tl"></div> <div class="td-tr"></div> <div class="td-bl"></div> <div class="td-br"></div>
+<div class="td-tc"></div> <div class="td-bc"></div> <div class="td-cl"></div> <div class="td-cr"></div>
+<div class="td-cc"></div> <div class="td-body">
+		<h1 id="firstHeading" class="firstHeading"><?php $this->data['displaytitle']!=""?$this->html('title'):$this->text('title') ?></h1>
+        <div id="bodyContentInnerWrapper">
+		<div id="bodyContent">
+			<h3 id="siteSub"><?php $this->msg('tagline') ?></h3>
+			<div id="contentSub"><?php $this->html('subtitle') ?></div>
+			<?php if($this->data['undelete']) { ?><div id="contentSub2"><?php     $this->html('undelete') ?></div><?php } ?>
+			<?php if($this->data['newtalk'] ) { ?><div class="usermessage"><?php $this->html('newtalk')  ?></div><?php } ?>
+			<?php if($this->data['showjumplinks']) { ?><div id="jump-to-nav"><?php $this->msg('jumpto') ?> <a href="#column-one"><?php $this->msg('jumptonavigation') ?></a>, <a href="#searchInput"><?php $this->msg('jumptosearch') ?></a></div><?php } ?>
+			<!-- start content -->
+			<?php $this->html('bodytext') ?>
+			<?php if($this->data['catlinks']) { $this->html('catlinks'); } ?>
+			<!-- end content -->
+			<?php if($this->data['dataAfterContent']) { $this->html ('dataAfterContent'); } ?>
+			<div class="visualClear"></div>
+            </div><!--//bodyContentInnerWrapper -->
+		</div>
+         </div><!--//td-body -->
+    </div><!--// td-roundedbox-->
+	</div>
+		</div>
+		<div id="column-one">
+	<div id="p-cactions" class="portlet">
+		<h5><?php $this->msg('views') ?></h5>
+		<div class="pBody">
+			<ul>
+	<?php		foreach($this->data['content_actions'] as $key => $tab) {
+					echo '
+				 <li id="' . Sanitizer::escapeId( "ca-$key" ) . '"';
+					if( $tab['class'] ) {
+						echo ' class="'.htmlspecialchars($tab['class']).'"';
+					}
+					echo'><a href="'.htmlspecialchars($tab['href']).'"';
+					# We don't want to give the watch tab an accesskey if the
+					# page is being edited, because that conflicts with the
+					# accesskey on the watch checkbox.  We also don't want to
+					# give the edit tab an accesskey, because that's fairly su-
+					# perfluous and conflicts with an accesskey (Ctrl-E) often
+					# used for editing in Safari.
+				 	if( in_array( $action, array( 'edit', 'submit' ) )
+				 	&& in_array( $key, array( 'edit', 'watch', 'unwatch' ))) {
+				 		echo $skin->tooltip( "ca-$key" );
+				 	} else {
+				 		echo Html::expandAttributes( Linker::tooltipAndAccesskeyAttribs( "ca-$key" ) );
+				 	}
+				 	echo '>'.htmlspecialchars($tab['text']).'</a></li>';
+				} ?>
+			</ul>
+		</div>
+	</div>
+	<div class="portlet" id="p-personal">
+		<h5><?php $this->msg('personaltools') ?></h5>
+		<div class="pBody">
+			<ul>
+<?php 			foreach($this->data['personal_urls'] as $key => $item) { ?>
+				<li id="<?php echo Sanitizer::escapeId( "pt-$key" ) ?>"<?php
+					if ($item['active']) { ?> class="active"<?php } ?>><a href="<?php
+				echo htmlspecialchars($item['href']) ?>"<?php echo Html::expandAttributes( Linker::tooltipAndAccesskeyAttribs( 'pt-'.$key ) ); ?><?php
+				if(!empty($item['class'])) { ?> class="<?php
+				echo htmlspecialchars($item['class']) ?>"<?php } ?>><?php
+				echo htmlspecialchars($item['text']) ?></a></li>
+<?php			} ?>
+			</ul>
+		</div>
+	</div>
+	<div class="portlet" id="p-logo">
+		<a style="background-image: url(<?php $this->text('logopath') ?>);" <?php
+			?>href="<?php echo htmlspecialchars($this->data['nav_urls']['mainpage']['href'])?>"<?php
+			echo Html::expandAttributes( Linker::tooltipAndAccesskeyAttribs( 'p-logo' ) ); ?>></a>
+	</div>
+	<script type="<?php $this->text('jsmimetype') ?>"> if (window.isMSIE55) fixalpha(); </script>
+
+<?php
+		$sidebar = $this->data['sidebar'];
+		$beginyellowtags="<div id=\"td-roundedbox\">
+<div class=\"td-tl-yel\"></div> <div class=\"td-tr-yel\"></div> <div class=\"td-bl-yel\"></div> <div class=\"td-br-yel\"></div>
+<div class=\"td-tc-yel\"></div> <div class=\"td-bc-yel\"></div> <div class=\"td-cl-yel\"></div> <div class=\"td-cr-yel\"></div>
+<div class=\"td-cc-yel\"></div> <div class=\"td-body-yel\">";
+$begingreentags="<div id=\"td-roundedbox\">
+<div class=\"td-tl-green\"></div> <div class=\"td-tr-green\"></div> <div class=\"td-bl-green\"></div> <div class=\"td-br-green\"></div>
+<div class=\"td-tc-green\"></div> <div class=\"td-bc-green\"></div> <div class=\"td-cl-green\"></div> <div class=\"td-cr-green\"></div>
+<div class=\"td-cc-green\"></div> <div class=\"td-body-green\">";
+		$endtags="</div><!--//td-body -->
+    </div><!--// td-roundedbox-->";
+		if ( !isset( $sidebar['SEARCH'] ) ) $sidebar['SEARCH'] = true;
+		if ( !isset( $sidebar['TOOLBOX'] ) ) $sidebar['TOOLBOX'] = true;
+		foreach ($sidebar as $boxName => $cont) {
+			if ( $boxName == 'SEARCH' ) {
+				$this->searchBox();
+			} elseif ( $boxName == 'TOOLBOX' ) {
+                 echo $begingreentags;
+				$this->toolbox();
+                 echo $endtags;
+			} elseif ( isset( $sidebar['LANGUAGES'] ) && $boxName == 'LANGUAGES' ) {
+                
+				$this->languageBox();
+             
+			} elseif ( !empty($boxName) ) {
+                 echo $beginyellowtags;
+				$this->customBox( $boxName, $cont );
+                 echo $endtags;
+			}
+		}
+?>
+
+
+		</div><!-- end of the left (by default at least) column -->
+			<div class="visualClear"></div>
+            <div id="td-roundedbox">
+<div class="td-tfl"></div> <div class="td-tfr"></div> <div class="td-bfl"></div> <div class="td-bfr"></div>
+<div class="td-tfc"></div> <div class="td-bfc"></div> <div class="td-cfl"></div> <div class="td-cfr"></div>
+<div class="td-cfc"></div> <div class="td-fbody">
+            
+			<div id="footer">
+<?php
+		if($this->data['poweredbyico']) { ?>
+				<div id="f-poweredbyico"><?php $this->html('poweredbyico') ?></div>
+<?php 	}
+		if($this->data['copyrightico']) { ?>
+				<div id="f-copyrightico"><?php $this->html('copyrightico') ?></div>
+<?php	}
+
+		// Generate additional footer links
+		$footerlinks = array(
+			'lastmod', 'viewcount', 'numberofwatchingusers', 'credits', 'copyright',
+			'privacy', 'about', 'disclaimer', 'tagline',
+		);
+		$validFooterLinks = array();
+		foreach( $footerlinks as $aLink ) {
+			if( isset( $this->data[$aLink] ) && $this->data[$aLink] ) {
+				$validFooterLinks[] = $aLink;
+			}
+		}
+		if ( count( $validFooterLinks ) > 0 ) {
+?>			<ul id="f-list">
+<?php
+			foreach( $validFooterLinks as $aLink ) {
+				if( isset( $this->data[$aLink] ) && $this->data[$aLink] ) {
+?>					<li id="<?php echo$aLink?>"><?php $this->html($aLink) ?></li>
+<?php 			}
+			}
+?>
+			</ul>
+<?php	}
+?>
+		</div>
+</div>
+      </div><!--//td-fbody -->
+    </div><!--// td-roundedbox-->
+<?php $this->html('bottomscripts'); /* JS call to runBodyOnloadHook */ ?>
+<?php $this->html('reporttime') ?>
+<?php if ( $this->data['debug'] ): ?>
+<!-- Debug output:
+<?php $this->text( 'debug' ); ?>
+
+-->
+<?php endif; ?>
+</body></html>
+<?php
+	wfRestoreWarnings();
+	} // end of execute() method
+
+	/*************************************************************************************************/
+	function searchBox() {
+		global $wgUseTwoButtonsSearchForm;
+?>
+	<div id="p-search" class="portlet">
+		<h5><label for="searchInput"><?php $this->msg('search') ?></label></h5>
+		<div id="searchBody" class="pBody">
+			<form action="<?php $this->text('wgScript') ?>" id="searchform"><div>
+				<input type='hidden' name="title" value="<?php $this->text('searchtitle') ?>"/>
+				<img src="skins/roundedsassy/css/magnifyingglass.gif" alt="search" /><input id="searchInput" name="search" type="text"<?php echo Html::expandAttributes( Linker::tooltipAndAccesskeyAttribs( 'search' ) );
+					if( isset( $this->data['search'] ) ) {
+						?> value="<?php $this->text('search') ?>"<?php } ?> />
+				<input type='image' name="go" class="searchButton" id="searchGoButton"	value="<?php $this->msg('searcharticle') ?>" src="skins/roundedsassy/css/go.gif" <?php echo Html::expandAttributes( Linker::tooltipAndAccesskeyAttribs( 'search-go' ) ); ?> /><?php if ($wgUseTwoButtonsSearchForm) { ?>&nbsp;
+				<input type='image' name="fulltext" class="searchButton" id="mw-searchButton" value="<?php $this->msg('searchbutton') ?>" src="skins/roundedsassy/css/search.gif" <?php echo Html::expandAttributes( Linker::tooltipAndAccesskeyAttribs( 'search-fulltext' ) ); ?> /><?php } else { ?>
+
+				<div><a href="<?php $this->text('searchaction') ?>" rel="search"><?php $this->msg('powersearch-legend') ?></a></div><?php } ?>
+
+			</div></form>
+		</div>
+	</div>
+<?php
+	}
+
+	/*************************************************************************************************/
+	function toolbox() {
+?>
+	<div class="portlet" id="p-tb">
+		<h5><?php $this->msg('toolbox') ?></h5>
+		<div class="pBody">
+			<ul>
+<?php
+		if($this->data['notspecialpage']) { ?>
+				<li id="t-whatlinkshere"><a href="<?php
+				echo htmlspecialchars($this->data['nav_urls']['whatlinkshere']['href'])
+				?>"<?php echo Html::expandAttributes( Linker::tooltipAndAccesskeyAttribs( 't-whatlinkshere' ) ); ?>><?php $this->msg('whatlinkshere') ?></a></li>
+<?php
+			if( $this->data['nav_urls']['recentchangeslinked'] ) { ?>
+				<li id="t-recentchangeslinked"><a href="<?php
+				echo htmlspecialchars($this->data['nav_urls']['recentchangeslinked']['href'])
+				?>"<?php echo Html::expandAttributes( Linker::tooltipAndAccesskeyAttribs( 't-recentchangeslinked' ) ); ?>><?php $this->msg('recentchangeslinked') ?></a></li>
+<?php 		}
+		}
+		if(isset($this->data['nav_urls']['trackbacklink'])) { ?>
+			<li id="t-trackbacklink"><a href="<?php
+				echo htmlspecialchars($this->data['nav_urls']['trackbacklink']['href'])
+				?>"<?php echo Html::expandAttributes( Linker::tooltipAndAccesskeyAttribs( 't-trackbacklink' ) ); ?>><?php $this->msg('trackbacklink') ?></a></li>
+<?php 	}
+		if($this->data['feeds']) { ?>
+			<li id="feedlinks"><?php foreach($this->data['feeds'] as $key => $feed) {
+					?><a id="<?php echo Sanitizer::escapeId( "feed-$key" ) ?>" href="<?php
+					echo htmlspecialchars($feed['href']) ?>" rel="alternate" type="application/<?php echo $key ?>+xml" class="feedlink"<?php echo Html::expandAttributes( Linker::tooltipAndAccesskeyAttribs( 'feed-'.$key ) ); ?>><?php echo htmlspecialchars($feed['text'])?></a>&nbsp;
+					<?php } ?></li><?php
+		}
+
+		foreach( array('contributions', 'log', 'blockip', 'emailuser', 'upload', 'specialpages') as $special ) {
+
+			if($this->data['nav_urls'][$special]) {
+				?><li id="t-<?php echo $special ?>"><a href="<?php echo htmlspecialchars($this->data['nav_urls'][$special]['href'])
+				?>"<?php echo Html::expandAttributes( Linker::tooltipAndAccesskeyAttribs( 't-'.$special ) ); ?>><?php $this->msg($special) ?></a></li>
+<?php		}
+		}
+
+		if(!empty($this->data['nav_urls']['print']['href'])) { ?>
+				<li id="t-print"><a href="<?php echo htmlspecialchars($this->data['nav_urls']['print']['href'])
+				?>" rel="alternate"<?php echo Html::expandAttributes( Linker::tooltipAndAccesskeyAttribs( 't-print' ) ); ?>><?php $this->msg('printableversion') ?></a></li><?php
+		}
+
+		if(!empty($this->data['nav_urls']['permalink']['href'])) { ?>
+				<li id="t-permalink"><a href="<?php echo htmlspecialchars($this->data['nav_urls']['permalink']['href'])
+				?>"<?php echo Html::expandAttributes( Linker::tooltipAndAccesskeyAttribs( 't-permalink' ) ); ?>><?php $this->msg('permalink') ?></a></li><?php
+		} elseif ($this->data['nav_urls']['permalink']['href'] === '') { ?>
+				<li id="t-ispermalink"<?php echo $this->skin->tooltip('t-ispermalink') ?>><?php $this->msg('permalink') ?></li><?php
+		}
+
+		wfRunHooks( 'roundedsassyTemplateToolboxEnd', array( &$this ) );
+		wfRunHooks( 'SkinTemplateToolboxEnd', array( &$this ) );
+?>
+			</ul>
+		</div>
+	</div>
+<?php
+	}
+
+	/*************************************************************************************************/
+	function languageBox() {
+		if( $this->data['language_urls'] ) {
+?>
+	<div id="p-lang" class="portlet">
+		<h5><?php $this->msg('otherlanguages') ?></h5>
+		<div class="pBody">
+			<ul>
+<?php		foreach($this->data['language_urls'] as $langlink) { ?>
+				<li class="<?php echo htmlspecialchars($langlink['class'])?>"><?php
+				?><a href="<?php echo htmlspecialchars($langlink['href']) ?>"><?php echo $langlink['text'] ?></a></li>
+<?php		} ?>
+			</ul>
+		</div>
+	</div>
+<?php
+		}
+	}
+
+	/*************************************************************************************************/
+	function customBox( $bar, $cont ) {
+?>
+	<div class='generated-sidebar portlet' id='<?php echo Sanitizer::escapeId( "p-$bar" ) ?>'<?php echo $this->skin->tooltip('p-'.$bar) ?>>
+		<h5><?php $out = wfMsg( $bar ); if (wfEmptyMsg($bar, $out)) echo $bar; else echo $out; ?></h5>
+		<div class='pBody'>
+<?php   if ( is_array( $cont ) ) { ?>
+			<ul>
+<?php 			foreach($cont as $key => $val) { ?>
+				<li id="<?php echo Sanitizer::escapeId($val['id']) ?>"<?php
+					if ( $val['active'] ) { ?> class="active" <?php }
+				?>><a href="<?php echo htmlspecialchars($val['href']) ?>"<?php echo Html::expandAttributes( Linker::tooltipAndAccesskeyAttribs( $val['id'] ) ); ?>><?php echo htmlspecialchars($val['text']) ?></a></li>
+<?php			} ?>
+			</ul>
+<?php   } else {
+			# allow raw HTML block to be defined by extensions
+			print $cont;
+		}
+?>
+		</div>
+	</div>
+<?php
+	}
+
+} // end of class
+
+
